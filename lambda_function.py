4f1ca53cd01350b35d16590ae24635bb155e09ce
Behdad Ehsani <58978680+behdad13@users.noreply.github.com>
2023-05-31 22:37:16 -0400

    v 1.0.1

--- lambda_function.py
+++ lambda_function.py
@@ -10,7 +10,6 @@ def lambda_handler(event, context):
     environ = os.environ.get('ENVIRON')
     if environ == 'DEV':
         print(f'Running in {environ} environment')
-        os.environ.setdefault('AWS_PROFILE', 'itvgithub')
     bucket_name = os.environ.get('BUCKET_NAME')
     bookmark_file = os.environ.get('BOOKMARK_FILE')
     baseline_file = os.environ.get('BASELINE_FILE')
