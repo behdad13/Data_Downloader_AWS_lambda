570f55d520686e89fa166f0d71419f7c1587bb67
Behdad Ehsani <58978680+behdad13@users.noreply.github.com>
2023-05-31 22:38:26 -0400

    v 1.0.1

--- util.py
+++ util.py
@@ -1,6 +1,4 @@
-from datetime import datetime as dt
-from datetime import timedelta as td
-import requests, boto3, os
+import boto3
 from botocore.errorfactory import ClientError
 
 
@@ -32,8 +30,26 @@ def upload_bookmark(bucket, file_prefix, bookmark_file, bookmark_contents):
         Body=bookmark_contents.encode('utf-8')
     )
 
+from datetime import datetime, timedelta
+
+from datetime import datetime, timedelta
 
 def get_next_file_name(prev_file):
-    dt_part = prev_file.split('.')[0]
-    next_file = f"{dt.strftime(dt.strptime(dt_part, '%Y-%M-%d-%H') + td(hours=1), '%Y-%M-%d-%-H')}.json.gz"
+    date_part = prev_file.split('_')[2].split('.')[0]
+    year = int(date_part[:4])
+    month = int(date_part[4:6])
+    day = int(date_part[6:8])
+    hour = int(date_part[8:10])
+
+    # If the hour is 24, we consider it as the beginning of the next day, and the hour should be 1 (for the next file)
+    if hour == 24:
+        current_datetime = datetime(year, month, day) + timedelta(days=1) # increment the day
+        formatted_hour = '01' # start from 1 for the next day
+    else:
+        # Otherwise, increment the hour and maintain the day
+        current_datetime = datetime(year, month, day) + timedelta(hours=hour)
+        formatted_hour = f"{current_datetime.hour + 1:02d}" # increment the hour, ensure it's two digits
+
+    next_file = f"PUB_RealtimeMktTotals_{current_datetime.strftime('%Y%m%d')}{formatted_hour}.csv"
     return next_file
+
